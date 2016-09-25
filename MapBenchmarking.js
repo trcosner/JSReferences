8f00081c7eb8e7c36708cd5e0708dd59eaba7896
trcosner <trcosner@gmail.com>
2016-09-25 16:17:54 -0400

    logging exercises and derivations of map function

---
+++ MapBenchmarking.js
@@ -0,0 +1,48 @@
+var data = [];
+
+for(var i = 0; i < 10000; i++){
+  var object = {
+    val1: i,
+    val2: i%2,
+    val3: i%5,
+    val4: i%7
+  }
+  data.push(object);
+}
+
+//forEach Implementation of Map
+var array = [];
+
+console.time('forEach Map')
+data.forEach(function(item){
+    var obj = {
+      id: item.val1,
+      modTwo: item.val2
+    };
+    array.push(obj);
+});
+console.timeEnd('forEach Map');
+
+//Reduce Implementation of Map
+console.time('Reduce Map');
+data.reduce(function(acc, item){
+  obj = {
+    id: item.val1,
+    modTwo: item.val2
+  };
+  acc.push(obj);
+
+  return acc;
+}, [])
+console.timeEnd('Reduce Map');
+
+
+//Standard Implementation of Map
+console.time('map');
+data.map(function(item){
+  return {
+    id: item.val1,
+    modTwo: item.val2
+  };
+});
+console.timeEnd('map');
---
+++ logging.js
@@ -0,0 +1,14 @@
+//Five Baic Console Logs
+console.log('Basic log.');
+console.warn('Console warning.');
+console.error('Console error.');
+console.info('Console info.');
+console.debug('Basic debug');
+
+//String cohercion
+//first arg string with %s reads second arg as String
+console.log('I am %s years old' , 23);
+
+//console css
+//first argument string with %c reads second arg as css
+console.log('Fun with %cCSS', 'color: blue;')
